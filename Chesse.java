7bb6632f82b16b7283bfc9bf915e62b6776b8a68
ruzicka <annonymause@email.cz>
2024-08-22 12:24:59 +0200

    format Moves.java

--- Chesse.java
+++ Chesse.java
@@ -9,6 +9,8 @@ import java.util.Map;
 public class Chesse {
 
     //--------------------------------------------------------------------------------
+    private  Moves moves;
+    //--------------------------------------------------------------------------------
 
     public static void init() {
         int size_board = 6;
@@ -31,91 +33,7 @@ public class Chesse {
         int[] start_horse = board.get("(0,0)");
     }
 
-    //--------------------------------------------------------------------------------
 
-    public static class ResultPair {
-        private final int x;
-        private final int y;
-
-        public ResultPair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        @Override
-        public String toString() {
-            return "(" + x + ", " + y + ")";
-        }
-    }
-
-    //--------------------------------------------------------------------------------
-
-    public static void movexmore(int[] start, int[] end) {
-        int x = Math.abs(start[0] - end[0]);
-        int y = Math.abs(start[1] - end[1]);
-        System.out.println("x: " + x + " y: " + y);
-    }
-
-    public enum Direction {
-        UP_LEFT {
-            @Override
-            public ResultPair apply(int x, int y) {
-                return new ResultPair(x - 1, y + 2);
-            }
-        },
-        UP_RIGHT {
-            @Override
-            public ResultPair apply(int x, int y) {
-                return new ResultPair(x + 1, y + 2);
-            }
-        },
-        RIGHT_UP {
-            @Override
-            public ResultPair apply(int x, int y) {
-                return new ResultPair(x + 2, y + 1);
-            }
-        },
-        RIGHT_DOWN {
-            @Override
-            public ResultPair apply(int x, int y) {
-                return new ResultPair(x + 2, y - 1);
-            }
-        },
-        DOWN_LEFT {
-            @Override
-            public ResultPair apply(int x, int y) {
-                return new ResultPair(x - 1, y - 2);
-            }
-        },
-        DOWN_RIGHT {
-            @Override
-            public ResultPair apply(int x, int y) {
-                return new ResultPair(x + 1, y - 2);
-            }
-        },
-        LEFT_UP {
-            @Override
-            public ResultPair apply(int x, int y) {
-                return new ResultPair(x - 2, y + 1);
-            }
-        },
-        LEFT_DOWN {
-            @Override
-            public ResultPair apply(int x, int y) {
-                return new ResultPair(x - 2, y - 1);
-            }
-        };
-
-        public abstract ResultPair apply(int x, int y);
-    }
 
     //--------------------------------------------------------------------------------
 
---
+++ Moves.java
@@ -0,0 +1,85 @@
+/**
+ * @author ruzicka
+ * @since 2024-08-22
+ */
+public class Moves {
+    //--------------------------------------------------------------------------------
+
+    public static class ResultPair {
+        private final int x;
+        private final int y;
+
+        public ResultPair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
+    }
+
+    //--------------------------------------------------------------------------------
+
+    public enum Direction {
+        UP_LEFT {
+            @Override
+            public ResultPair apply(int x, int y) {
+                return new ResultPair(x - 1, y + 2);
+            }
+        },
+        UP_RIGHT {
+            @Override
+            public ResultPair apply(int x, int y) {
+                return new ResultPair(x + 1, y + 2);
+            }
+        },
+        RIGHT_UP {
+            @Override
+            public ResultPair apply(int x, int y) {
+                return new ResultPair(x + 2, y + 1);
+            }
+        },
+        RIGHT_DOWN {
+            @Override
+            public ResultPair apply(int x, int y) {
+                return new ResultPair(x + 2, y - 1);
+            }
+        },
+        DOWN_LEFT {
+            @Override
+            public ResultPair apply(int x, int y) {
+                return new ResultPair(x - 1, y - 2);
+            }
+        },
+        DOWN_RIGHT {
+            @Override
+            public ResultPair apply(int x, int y) {
+                return new ResultPair(x + 1, y - 2);
+            }
+        },
+        LEFT_UP {
+            @Override
+            public ResultPair apply(int x, int y) {
+                return new ResultPair(x - 2, y + 1);
+            }
+        },
+        LEFT_DOWN {
+            @Override
+            public ResultPair apply(int x, int y) {
+                return new ResultPair(x - 2, y - 1);
+            }
+        };
+
+        public abstract ResultPair apply(int x, int y);
+    }
+}
