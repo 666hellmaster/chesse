d571e55567b8eef7d0025b9d59dd7f03e1871848
ruzicka <annonymause@email.cz>
2024-08-22 09:47:35 +0200

    INIT

---
+++ chesse.java
@@ -0,0 +1,10 @@
+/**
+ * @author ruzicka
+ * @since 2024-08-22
+ */
+public class chesse {
+     //--------------------------------------------------------------------------------
+    int size_board = 6;
+    int[][] board = new int[size_board][size_board];
+
+}
